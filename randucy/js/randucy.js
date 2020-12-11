bb21b7c70fb00785af372686aeaa7a59387a8479
Thijs Boom <twboom@hotmail.nl>
2020-12-12 00:37:10 +0100

    Got the writing working, kinda

--- randucy/js/randucy.js
+++ randucy/js/randucy.js
@@ -1,8 +1,19 @@
-function newLine() {
-  let input = document.querySelector('#input')
+var input = document.querySelector('#input');
 
-    document.write(input);
-};
+function newLine () {
+  // create a new div element
+  const newDiv = document.createElement('p');
+
+  // and give it some content
+  const newContent = document.createTextNode(input.value);
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById('p');
+  document.body.insertBefore(newDiv, currentDiv);
+}
 
 document.getElementById("input").onkeypress = function(event){
                 if (event.keyCode == 13 || event.which == 13){
