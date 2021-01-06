ddbd5dab4a0e1f666a1669ae52f4b541252074db
Thijs Boom <twboom@hotmail.nl>
2021-01-06 10:16:45 +0100

    Added more data

--- app/assets/data.js
+++ app/assets/data.js
@@ -4,3 +4,10 @@ const clue3 = {name: 'clue 3', description: 'clue 3 text'};
 const clue4 = {name: 'clue 4', description: 'clue 4 text'};
 const clue5 = {name: 'clue 5', description: 'clue 5 text'};
 const clue6 = {name: 'clue 6', description: 'clue 6 text'};
+
+const quiz1 = {name: 'quiz 1', description: 'quiz 1 desc', answer: 'quiz 1 ans'};
+const quiz2 = {name: 'quiz 2', description: 'quiz 2 desc', answer: 'quiz 2 ans'};
+const quiz3 = {name: 'quiz 3', description: 'quiz 3 desc', answer: 'quiz 3 ans'};
+const quiz4 = {name: 'quiz 4', description: 'quiz 4 desc', answer: 'quiz 4 ans'};
+const quiz5 = {name: 'quiz 5', description: 'quiz 5 desc', answer: 'quiz 5 ans'};
+const quiz6 = {name: 'quiz 6', description: 'quiz 6 desc', answer: 'quiz 6 ans'};
