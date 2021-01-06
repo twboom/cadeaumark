47ed976c7422a904ca79f3c835a9e13a9ebec030
Thijs Boom <twboom@hotmail.nl>
2021-01-06 10:10:26 +0100

    Made the clue list working

---
+++ app/assets/clue.js
@@ -0,0 +1,6 @@
+const clue1 = {name: 'clue 1', description: 'clue 1 text'};
+const clue2 = {name: 'clue 2', description: 'clue 2 text'};
+const clue3 = {name: 'clue 3', description: 'clue 3 text'};
+const clue4 = {name: 'clue 4', description: 'clue 4 text'};
+const clue5 = {name: 'clue 5', description: 'clue 5 text'};
+const clue6 = {name: 'clue 6', description: 'clue 6 text'};
--- app/functions/hunt.js
+++ app/functions/hunt.js
@@ -1,11 +1,72 @@
 // find <nr> [check if clues are unlocked]
 // clue <nr> <validator> = unlocks clue
 // clue list = list clues
+var clue1state = true;
+var clue2state = true;
+var clue3state = false;
+var clue4state = true;
+var clue5state = true;
+var clue6state = false;
+
 
 function find() {
 
 };
 
 function clue() {
+  let args = input.value.slice(4).trim().split(/ +/g)
+  console.log(args)
 
+  if (args == 'list') {
+    printClueList();
+  }
+}
+
+
+
+
+
+
+function printClueList() {
+  if (clue1state == true) {
+    newCommandLine(clue1.name + ':' + ' ' + clue1.description)
+  }
+  else if (clue1state == false) {
+    newCommandLine(clue1.name + ':' + ' ' + 'is locked')
+  }
+
+  if (clue2state == true) {
+    newCommandLine(clue2.name + ':' + ' ' + clue2.description)
+  }
+  else if (clue2state == false) {
+    newCommandLine(clue2.name + ':' + ' ' + 'is locked')
+  }
+
+  if (clue3state == true) {
+    newCommandLine(clue3.name + ':' + ' ' + clue3.description)
+  }
+  else if (clue3state == false) {
+    newCommandLine(clue3.name + ':' + ' ' + 'is locked')
+  }
+
+  if (clue4state == true) {
+    newCommandLine(clue4.name + ':' + ' ' + clue4.description)
+  }
+  else if (clue4state == false) {
+    newCommandLine(clue4.name + ':' + ' ' + 'is locked')
+  }
+
+  if (clue5state == true) {
+    newCommandLine(clue5.name + ':' + ' ' + clue5.description)
+  }
+  else if (clue5state == false) {
+    newCommandLine(clue5.name + ':' + ' ' + 'is locked')
+  }
+
+  if (clue6state == true) {
+    newCommandLine(clue6.name + ':' + ' ' + clue6.description)
+  }
+  else if (clue6state == false) {
+    newCommandLine(clue6.name + ':' + ' ' + 'is locked')
+  }
 }
