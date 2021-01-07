d1368e369da8736a82191b0457f3a1066825d078
Thijs Boom <twboom@hotmail.nl>
2021-01-07 09:56:21 +0100

    Made help screen

--- app/functions/help.js
+++ app/functions/help.js
@@ -1,3 +1,8 @@
 function help() {
-
+  newCommandLine('-------: Markinal Help Menu :-------')
+  newCommandLine('help: Shows you this screen')
+  newCommandLine('login: Logging in in Markinal, usage: [login <username> <password>]')
+  newCommandLine('logout: Logs you out of Markinal')
+  newCommandLine('clue list: Lists the clues')
+  newCommandLine('clue <clueNumber> <quizAnswer>')
 }
