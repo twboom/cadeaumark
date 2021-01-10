b7283855ff5deb1fb4ca970f0b2f248bd0481324
Thijs Boom <twboom@hotmail.nl>
2021-01-10 10:40:18 +0100

    yes

--- app/assets/data.js
+++ app/assets/data.js
@@ -4,7 +4,7 @@ const clue3 = {name: 'Clue 3', description: 'Hier mag van jou niemand komen', pl
 const clue4 = {name: 'Clue 4', description: 'Hier snakken de konijnen naar', place: "Konijnen spullen"};
 const clue5 = {name: 'Clue 5', description: 'Het klinkt als muziek in je oren', place: "Tussen partituren"};
 const clue6 = {name: 'Clue 6', description: 'Beste uitzicht', place: "Zolder, kozijn achter"};
-const clue7 = {name: 'Clue 7, End clue', description: 'Het is dichtbij en jij kan erbij', place: "Op de boekenkast"}
+const clue7 = {name: 'Clue 7, End clue', description: 'Het is dichtbij en alleen jij kan erbij', place: "Op de boekenkast"}
 
 const quiz1 = {name: 'HTTP Response Status Code', description: 'quiz 1 desc', answer: 'NMIIA1301408511410100'};
 const quiz2 = {name: 'Duitse Filippine', description: 'quiz 2 desc', answer: 'markeuaw'};
